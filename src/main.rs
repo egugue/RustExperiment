9ad3413630756b12d29a128c2d59b46d0b168480
Haruyoshi Toyama <duane0728@gmail.com>
2020-12-30 23:36:04 +0900

    initialize

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+pub mod officialbook;
+
+fn main() {
+    println!("Hello, world!");
+    return;
+}
---
+++ src/officialbook.rs
@@ -0,0 +1,2 @@
+/// https://doc.rust-lang.org/edition-guide/rust-2018/module-system/path-clarity.html#no-more-modrs
+pub mod _7;
