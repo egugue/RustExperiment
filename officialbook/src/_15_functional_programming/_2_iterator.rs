c368a98d9c3309e52a1b63fbbf7bd29588597109
Haruyoshi Toyama <duane0728@gmail.com>
2021-01-14 00:46:12 +0900

    Add The Iterator Trait and the next Method

--- officialbook/src/_15_functional_programming/_2_iterator.rs
+++ officialbook/src/_15_functional_programming/_2_iterator.rs
@@ -1,6 +1,8 @@
 pub fn main() {
     utils::println_file_name!();
     calling_order();
+    iterator_demonstration();
+    iter_methods();
 }
 
 fn calling_order() {
@@ -21,4 +23,34 @@ fn calling_order() {
             true
         })
         .for_each(|x| println!("x3 = {}.", x));
+}
+
+/// https://doc.rust-lang.org/book/ch13-02-iterators.html#the-iterator-trait-and-the-next-method
+fn iterator_demonstration() {
+    utils::println_function_name!();
+
+    let vec = vec![10, 20, 30];
+    let mut iter = vec.iter();
+    assert_eq!(iter.next(), Some(&10));
+    assert_eq!(iter.next(), Some(&20));
+    assert_eq!(iter.next(), Some(&30));
+    assert_eq!(iter.next(), None);
+    assert_eq!(vec, vec![10, 20, 30])
+}
+
+// the variety of iter creation
+fn iter_methods() {
+    utils::println_function_name!();
+
+    let vec = vec!["a".to_string()];
+    let mut iter = vec.into_iter();
+    assert_eq!(iter.next(), Some("a".to_string()));
+     // cannot compile because vec moved to iter due to into_iter call
+    // assert_eq!(vec, vec!["a".to_string()]);
+
+    // define as mut to call iter_mut
+    let mut vec = vec!["a".to_string()];
+    let mut iter = vec.iter_mut();
+    iter.next().unwrap().push('b');
+    assert_eq!(vec, vec!["ab"]);
 }
