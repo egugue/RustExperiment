47327e5b9b1a83d0fa4186c838b58451e0915aa8
Haruyoshi Toyama <duane0728@gmail.com>
2021-01-19 03:02:49 +0900

    Drop manually

--- officialbook/src/_15_smart_pointer/_3_drop.rs
+++ officialbook/src/_15_smart_pointer/_3_drop.rs
@@ -2,6 +2,7 @@
 pub fn main() {
     utils::println_file_name!();
     check_drop();
+    invoking_drop_manually();
 }
 
 #[derive(Debug)]
@@ -11,7 +12,7 @@ struct CustomSmartPointer {
 
 impl Drop for CustomSmartPointer {
     fn drop(&mut self) {
-        println!("about to drop: {:?}", self)
+        println!("dropping: {:?}", self)
     }
 }
 
@@ -29,3 +30,20 @@ fn check_drop() {
     // about to drop: CustomSmartPointer { data: "1" }
 }
 
+/// To drop manually, we must use std::mem::drop.
+///
+/// Because if a drop method is invoked directly, Rust will also call the drop method automatically,
+/// then a double free error occurs.
+fn invoking_drop_manually() {
+    utils::println_function_name!();
+    let a = CustomSmartPointer { data: "1".to_string() };
+    // a.drop();
+    //   ^^^^
+    //   |
+    //   explicit destructor calls not allowed
+    //   help: consider using `drop` function: `drop(a)`
+
+    println!("CustomSmartPointers is created.");
+    std::mem::drop(a);
+    println!("CustomSmartPointer is about to go out of scope");
+}
