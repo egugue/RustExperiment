e0b2063a858b898823a5442bdd10c3adad1734bc
Haruyoshi Toyama <duane0728@gmail.com>
2021-01-20 02:45:41 +0900

    Reference Counter

--- officialbook/src/_15_smart_pointer.rs
+++ officialbook/src/_15_smart_pointer.rs
@@ -1,9 +1,11 @@
 mod _1_box;
 mod _2_deref;
 mod _3_drop;
+mod _4_rc;
 
 pub fn main() {
     _1_box::main();
     _2_deref::main();
     _3_drop::main();
+    _4_rc::main();
 }
---
+++ officialbook/src/_15_smart_pointer/_4_rc.rs
@@ -0,0 +1,42 @@
+use std::rc::Rc;
+
+pub fn main() {
+    utils::println_file_name!();
+    rc_can_be_connected_with_others();
+}
+
+enum InvalidMultiConnectedList {
+    Cons(i32, Box<InvalidMultiConnectedList>),
+    Nil,
+}
+
+fn cons_cannot_be_connected_with_others() {
+    // use self::InvalidMultiConnectedList::{Cons, Nil};
+    // let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    // let b = Cons(3, Box::new(a));
+    // let c = Cons(4, Box::new(a));
+}
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+/// Actually a.clone() can be used. But Rust's convention is to use Rc::clone().
+/// Because if we find clone() method, we usually guess it does deep copy.
+/// To reduce misunderstanding, we should use Rc::clone() explicitly.
+///
+/// https://doc.rust-lang.org/book/ch15-04-rc.html#using-rct-to-share-data
+fn rc_can_be_connected_with_others() {
+    utils::println_function_name!();
+
+    use self::List::{Cons, Nil};
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
+
+    println!("a = {:?}", a);
+    println!("b = {:?}", b);
+    println!("c = {:?}", c);
+}
