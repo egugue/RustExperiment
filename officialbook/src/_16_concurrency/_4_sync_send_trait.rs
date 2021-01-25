92b305452c8578cb5d65060bd5dc5bbc6b3af5ef
Haruyoshi Toyama <duane0728@gmail.com>
2021-01-26 02:28:50 +0900

    Sync

--- officialbook/src/_16_concurrency/_4_sync_send_trait.rs
+++ officialbook/src/_16_concurrency/_4_sync_send_trait.rs
@@ -1,9 +1,12 @@
+use std::cell::Cell;
 use std::rc::Rc;
+use std::sync::Mutex;
 use std::thread;
 
 pub fn main() {
     utils::println_file_name!();
     send_trait();
+    sync_trait();
 }
 
 const STATIC_POINTER: &'static str = "static pointer";
@@ -22,11 +25,41 @@ fn send_trait() {
 
         println!("a raw pointer is not Send");
         // pointer;
-        println!("a tatic pointer is Send: {}", STATIC_POINTER);
+        println!("a static pointer is Send: {}", STATIC_POINTER);
 
         println!("Rc type is not Send");
         // rc.borrow();
     });
 
     handle.join().unwrap();
+}
+
+/// see https://doc.rust-lang.org/std/marker/trait.Sync.html for the details.
+fn sync_trait() {
+    utils::println_function_name!();
+
+    let primitive = 1;
+    check_sync(primitive);
+
+    let pointer: &i32 = &1;
+    check_sync(pointer);
+
+    let string: String = String::from("str");
+    check_sync(string);
+
+    let mutex: Mutex<i32> = Mutex::new(1);
+    check_sync(mutex);
+
+    let rc: Rc<i32> = Rc::new(1);
+    // check_sync(rc);
+
+    // > Types that are not Sync are those that have "interior mutability" in a non-thread-safe form, such as Cell and RefCell.
+    let cell: Cell<i32> = Cell::new(1);
+    // check_sync(cell);
+}
+
+fn check_sync<T>(v: T)
+where
+    T: Sync,
+{
 }
