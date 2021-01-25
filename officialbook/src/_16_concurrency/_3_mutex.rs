49450d0b681719495031656fe996d1a5b5585f07
Haruyoshi Toyama <duane0728@gmail.com>
2021-01-26 01:20:23 +0900

    Case of using Arc

--- officialbook/src/_16_concurrency/_3_mutex.rs
+++ officialbook/src/_16_concurrency/_3_mutex.rs
@@ -1,11 +1,12 @@
 use std::rc::Rc;
-use std::sync::{Mutex, MutexGuard};
+use std::sync::{Arc, Mutex, MutexGuard};
 use std::thread;
 
 pub fn main() {
     utils::println_file_name!();
     mutex_on_single_thread();
-    mutex_on_multiple_thread_with_rc();
+    // mutex_on_multiple_thread_with_rc();
+    mutex_on_multiple_thread_with_arc();
 }
 
 fn mutex_on_single_thread() {
@@ -29,7 +30,27 @@ fn mutex_on_multiple_thread_with_rc() {
     for _ in 0..10 {
         let counter = Rc::clone(&counter);
         let handle = thread::spawn(move || {
+            // This cannot compile.
             // Rc cannot be used between thread because it doesn't implement Send trait.
+            // let mut num = counter.lock().unwrap();
+            // *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for h in handles {
+        h.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
+
+fn mutex_on_multiple_thread_with_arc() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
             *num += 1;
         });
