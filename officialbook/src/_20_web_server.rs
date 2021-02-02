abd985519ef5cb153eb2043e833bc7da06f9e595
Haruyoshi Toyama <duane0728@gmail.com>
2021-02-02 21:28:11 +0900

    WebServer

---
+++ officialbook/src/_20_web_server.rs
@@ -0,0 +1,5 @@
+mod server;
+
+pub fn main() {
+    server::main();
+}
--- officialbook/src/lib.rs
+++ officialbook/src/lib.rs
@@ -9,11 +9,13 @@ pub mod _10_generic_type_traits_lifetimes;
 pub mod _13_functional_programming;
 mod _15_smart_pointer;
 mod _16_concurrency;
+mod _20_web_server;
 
 pub fn main() {
     // _07_dir::code_in_dir::function_in_dir();
     // _10::main();
     // _13_functional_programming::main();
     // _15_smart_pointer::main();
-    _16_concurrency::main();
+    // _16_concurrency::main();
+    _20_web_server::main();
 }
