93506cd1fcbaa264e7494c3c79a3b1e4e0118a97
Haruyoshi Toyama <duane0728@gmail.com>
2021-02-04 21:32:38 +0900

    ThreadPool

--- officialbook/src/_20_web_server/server.rs
+++ officialbook/src/_20_web_server/server.rs
@@ -5,10 +5,13 @@ use std::{fs, thread};
 
 pub fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let thread_pool = ThreadPool::new(4);
     println!("Started");
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        thread_pool.execute(move || {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -32,4 +35,30 @@ fn handle_connection(mut stream: TcpStream) {
     let response = format!("{}{}", status_line, contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+struct ThreadPool;
+
+impl ThreadPool {
+    /// Create a new ThreadPool
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    /// The functin will panic if size is zero.
+    fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        ThreadPool
+    }
+
+    /// FnOnce trait bound: the closure will be executed once in a thread.
+    /// Send  trait bound: the closure should transfer one thread to another thread.
+    /// static lifetime bound: we don't know how long the closure will execute.
+    ///
+    /// () after FnOnce means the closure takes no parameters and returns the unit type.
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
 }
