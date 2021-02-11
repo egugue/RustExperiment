315de7a81e6e3ee2617274b97173a4c05b602eb6
Haruyoshi Toyama <duane0728@gmail.com>
2021-02-11 23:06:54 +0900

    Extract writing to stdout into a function

--- regular_commands/src/bin/cat.rs
+++ regular_commands/src/bin/cat.rs
@@ -1,3 +1,4 @@
+use std::borrow::Borrow;
 use std::fs::File;
 use std::io::{BufReader, Read, Write};
 use std::{env, io};
@@ -7,16 +8,17 @@ fn main() {
     // let path = args.get(1).expect("Specify at least one file");
     // let mut f = File::open(path).expect(format!("{}: No such file", path).as_str());
 
-    let mut stdin = io::stdin();
+    write_to_stdout(io::stdin());
+}
+
+fn write_to_stdout<T: Read>(mut reader: T) {
     let mut stdout = io::stdout();
     let mut buffer = [0; 1024 * 4];
     loop {
-        let size = stdin.read(&mut buffer[..]).expect("failed to read");
+        let size = reader.read(&mut buffer[..]).expect("failed to read");
         if size == 0 {
             break;
         }
         stdout.write(&buffer[..size]).expect("failed to write");
     }
-
-    // println!("cat: {:?}", path);
 }
