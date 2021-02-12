233436fa46f8a5feff936bd58b758c859005ca4e
Haruyoshi Toyama <duane0728@gmail.com>
2021-02-12 23:24:40 +0900

    Remove command

--- mimic_commands/src/bin/wc_test.sh
+++ mimic_commands/src/bin/wc_test.sh
@@ -5,8 +5,6 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 pj_root_dir="$script_dir/../../../"
 cd "$pj_root_dir"
 
-pwd
-
 cargo build --release --bin wc
 my_wc="./target/release/wc"
 command diff <($my_wc invalid_file 2>&1) <(wc invalid_file 2>&1)
